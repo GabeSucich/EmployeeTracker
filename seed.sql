850328237170a338c100a0228982920d52aa08eb
Gabe Sucich <gabe.sucich@gmail.com>
2020-07-20 17:05:07 -0700

    Creaing seed.sql initial database

---
+++ seed.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE departments (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id int,
+    FOREIGN KEY (department_id) REFERENCES departments(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHARC(30) NOT NULL,
+    role_id int NOT NULL,
+    manager_id int NULL,
+    FOREIGN KEY (role_id) REFERENCES departments(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
+    PRIMARY KEY (id)
+)
